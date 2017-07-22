6f579d64a925f17c4c58123bff44bd0bf24509ca
Faraway <zhiyuanhong@hustunique.com>
2017-07-23 01:40:00 +0800

    略微修改目录结构

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask_init import app
+
+import json
+import sys
+
+import user
+import nest
+import alarmclock
+
+@app.errorhandler(500)
+def internal_server_err(e):
+  return json.dumps({
+    'error': 'Unexpected error! Maybe the back-end programmer is the one who to be blame.',
+  }), 500, { 'content-type': 'application/json' }
+
+if __name__ == '__main__':
+  app.run(host = '127.0.0.1', port = '8081')
+
