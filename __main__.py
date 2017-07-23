1e6eee33b58691560fcec3af8ef0371ede87ac02
Faraway <zhiyuanhong@hustunique.com>
2017-07-23 13:23:11 +0800

    接口文档目前写好的接口都有了

--- __main__.py
+++ __main__.py
@@ -13,6 +13,12 @@ def internal_server_err(e):
     'error': 'Unexpected error! Maybe the back-end programmer is the one who to be blame.',
   }), 500, { 'content-type': 'application/json' }
 
+@app.errorhandler(405)
+def methods_not_allowed(e):
+  return json.dumps({
+    'error': 'Method not allowed!',
+  }), 405, {'content-type': 'application/json'}
+
 if __name__ == '__main__':
   app.run(host = '127.0.0.1', port = '8081')
 
